b613640dbf1bc03e7e49310a266a9d7d247f741e
roncelli.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2024-09-30 13:14:47 +0200

    Programma completo (si possono inserire prezzi negativi).

---
+++ src/Auto.java
@@ -0,0 +1,48 @@
+public class Auto {
+    private String marca;
+    private String modello;
+    private double prezzo;
+    public Auto(){
+        marca=null;
+        modello=null;
+        prezzo=0;
+    }
+    public Auto(String auto1){
+        marca=auto1;
+        modello=null;
+        prezzo=0;
+    }
+    public Auto(double prezzo1){
+        marca=null;
+        modello=null;
+        prezzo=prezzo1;
+    }
+    public Auto(String marca1,double prezzo1){
+        marca=marca1;
+        modello=null;
+        prezzo=prezzo1;
+    }
+    public Auto(double prezzo1,String modello1){
+        marca=null;
+        modello=modello1;
+        prezzo=prezzo1;
+    }
+    public void setMarca (String marca1){
+        marca=marca1;
+    }
+    public void setModello(String modello1){
+        modello=modello1;
+    }
+    public void setPrezzo(double prezzo1){
+        prezzo=prezzo1;
+    }
+    public String getMarca(){
+        return marca;
+    }
+    public String getModello(){
+        return modello;
+    }
+    public double getPrezzo(){
+        return prezzo;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner input=new Scanner(System.in);
+        Auto a1=new Auto();
+        System.out.print("Inserisci la marca della prima auto ");
+        a1.setMarca(input.nextLine());
+        System.out.print("Inserisci il modello della prima auto ");
+        a1.setModello(input.nextLine());
+        System.out.print("Inserisci il prezzo della prima auto ");
+        a1.setPrezzo(input.nextDouble());
+        System.out.println();
+        Auto a2=new Auto();
+        System.out.print("Inserisci la marca della seconda auto ");
+        a2.setMarca(input.next());
+        System.out.print("Inserisci il modello della seconda auto ");
+        a2.setModello(input.next());
+        System.out.print("Inserisci il prezzo della seconda auto ");
+        a2.setPrezzo(input.nextDouble());
+        System.out.println();
+        Auto a3=new Auto();
+        System.out.print("Inserisci la marca della terza auto ");
+        a3.setMarca(input.next());
+        System.out.print("Inserisci il modello della terza auto ");
+        a3.setModello(input.next());
+        System.out.print("Inserisci il prezzo della terza auto ");
+        a3.setPrezzo(input.nextDouble());
+        Auto a4=new Auto();
+        System.out.println();
+        System.out.print("Inserisci la marca della quarta auto ");
+        a4.setMarca(input.next());
+        System.out.print("Inserisci il modello della quarta auto ");
+        a4.setModello(input.next());
+        System.out.print("Inserisci il prezzo della quarta auto ");
+        a4.setPrezzo(input.nextDouble());
+        System.out.println();
+        System.out.println();
+        System.out.println("Auto 1: "+a1.getMarca()+", "+a1.getModello()+", "+"€ "+a1.getPrezzo());
+        System.out.println("Auto 2: "+a2.getMarca()+", "+a2.getModello()+", "+"€ "+a2.getPrezzo());
+        System.out.println("Auto 3: "+a3.getMarca()+", "+a3.getModello()+", "+"€ "+a3.getPrezzo());
+        System.out.println("Auto 1: "+a4.getMarca()+", "+a4.getModello()+", "+"€ "+a4.getPrezzo());
     }
 }
